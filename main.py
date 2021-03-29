9b4b539150b8e6b1e68143132955c2adb2a51171
Dania <dania.liu@ocsbstudent.ca>
2021-03-29 17:46:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Shows the title.
+print ("Calculating the Area and Circumference of a Circle!")
+# Sets variables to default.
+firstDiameter = 0
+secondDiameter = 0
+firstResult = 0
+secondResult = 0
+import math
+
+# Makes  calculations for area using user input.
+firstDiameter = input("Enter diameter to determine area: ")
+firstResult = float(math.pi) * ((float(firstDiameter) / 2) * (float(firstDiameter) / 2))
+# Displays answer.
+print("The area of your circle is:")
+print(firstResult)
+# Makes  calculations for circumference using user input.
+secondDiameter = input("Enter diameter to determine circumference: ")
+secondResult = 2 * float(math.pi) * (float(secondDiameter) / 2)
+#Displays answer.
+print("The circumference of your circle is:")
+print(secondResult)
